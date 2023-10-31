c62c5c0251eec90e238a70ea75ae9f8e89318a65
Ava Logan <ava2logan@gmail.com>
2023-10-31 12:49:32 +0000

    Perfected find_indexes: which returns all instances of the guess letter in the random_ word, while iterating them out. Perfected update_hidden_word which exchanges the corresponding - to the guessed letter. Added some checkpoints which check for accuracy which should be removed later

--- milestone_4.py
+++ milestone_4.py
@@ -10,23 +10,28 @@ class Hangman:
         self.num_lives = 5
         self.list_of_guesses = []
 
-    def find_indexes(self, guess, random_word):
+    def find_indexes(self, guess):
+        print('made it to the find index method')
         guess_letter_index_list = []
         while guess in self.random_word:
             guess_index = self.random_word.index(guess)
             guess_letter_index_list.append(guess_index)
             self.random_word[guess_index] = '-'
+            print('The indices of the guess is ', guess_letter_index_list)    #rm later - shows index of guessed word
+            print('The random word that you DONT KNOW has these letters left', self.random_word) #rm later - shows that all occurences of the guess have been acknowledged
         return guess_letter_index_list
 
-        
+    def update_hidden_word(self, guess):
+        for numbers in Hangman.find_indexes(self, guess):
+            self.word_guessed[numbers] = guess
+            print('So far the word youve guessed is',self.word_guessed) #format later - shows guess so far 
 
     def check_guess(self, guess):
         guess = guess.lower()
         if guess in self.random_word:
             print(f'Good guess! {guess} is in the word')
-        
-
-
+            Hangman.find_indexes(self, guess)
+            Hangman.update_hidden_word(self, guess)
         elif guess not in self.random_word:
             print(f'Sorry {guess} is not in the word')
         else: 
@@ -41,20 +46,25 @@ class Hangman:
                 print("You have already tried that letter")
             elif len(guess) == 1 and guess.isalpha() == True:
                 Hangman.check_guess(self, guess)
-                break
-                #self.list_of_guesses.append(guess)
+                self.num_lives -=1     
+                self.list_of_guesses.append(guess)
+                print(f'You have {self.num_lives} remaining')
+                print(f'You have so far guessed {self.list_of_guesses}')
             else: 
                 print('Something went wrong with the input')
     
     def what_went_wrong(self):
         print(self.random_word)
-        print(self.word_guessed[2])
+        print(type(self.random_word))
         print(self.word_guessed)
         print(self.num_letters)
         print(self.num_lives)
         print(self.list_of_guesses) 
 
 player_1 = Hangman('banana')
-
-player_1.what_went_wrong()
 #player_1.ask_for_input()
+#player_1.what_went_wrong()
+
+player_2 = Hangman('potato')
+player_2.what_went_wrong()
+player_2.ask_for_input()
